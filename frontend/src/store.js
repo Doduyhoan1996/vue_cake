5fb5e1e1ecedbb0c153891c7ee1107ff13d7a0d9
hoanDD <>
2021-06-30 08:52:25 +0700

    Revert "update store"
    This reverts commit df797c6e5dc827804841d43ec133cc3051c90aa4.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,42 +2,34 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as mutation_types from './store/mutation_type'
 
+Vue.use(Vuex)
+
 //IMPORT MODULES
 import setting from './store/modules/setting'
 
-Vue.use(Vuex)
-
-let app_state = {
-  app_user: null,
-  app_state: '',
-}
-
-let getters = {
-  CHANGE_PROFILE: state => {
-    return state.app_state;
-  }
-}
-
-let mutations = {
-  [mutation_types.APP_USER](state, data) {
-    state.app_user = data
-  },
-  SET_CHANGE_PROFILE: (state, payload) => {
-    state.app_state = payload;
-  }
-}
-
-let actions = {
-  setAppUser({ commit }, info) {
-    commit(mutation_types.APP_USER, info)
-  }
-}
-
 export default new Vuex.Store({
-  app_state,
-  getters,
-  mutations,
-  actions,
+  state: {
+    app_user: null,
+    app_state: '',
+  }, 
+  getters: {
+    CHANGE_PROFILE: state => {
+        return state.app_state;
+    }
+  },
+  mutations: {
+    [mutation_types.APP_USER](state, data) {
+      state.app_user = data
+    },
+    SET_CHANGE_PROFILE: (state, payload) => {
+        state.app_state = payload;
+    }
+  },
+  actions: {
+    setAppUser({commit}, info) {
+      commit(mutation_types.APP_USER, info)
+    }
+  },
   modules: {
     setting
   }
